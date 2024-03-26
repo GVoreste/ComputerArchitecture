d6e4e784ae8732e1f43fd297f2b7a6953c4c1cb6
Oreste Sciacqualegni <giulianoverrando@gmail.com>
2024-03-26 20:33:57 +0100

    changed comment from // to #

--- DOUBTS/ASM_SOURCE/ifstatement_alt.asm
+++ DOUBTS/ASM_SOURCE/ifstatement_alt.asm
@@ -1,11 +1,11 @@
-addi x1, x0, 1  // i=1;
-addi x2, x0, 3  // j=3;
-addi x3, x0, 80 // g=80;
-addi x4, x0, 45 // h=45;
-beq x1, x2, ELSE  // if(i==j){
-sub x6, x3, x4   // 	f=g-h;
-jal x5, END       // }else{
+    addi x1, x0, 1   # i=1;
+    addi x2, x0, 3   # j=3;
+    addi x3, x0, 80  # g=80;
+    addi x4, x0, 45  # h=45;
+    beq x1, x2, ELSE # if(i==j){
+    sub x6, x3, x4   #   f=g-h;
+    jal x5, END      # }else{
 ELSE:
-add x6,x3,s5     //	f=g+h;
+    add x6,x3,s5     #	f=g+h;
 END:
-jal x5, END	  // }
+    jal x5, END	     # }
